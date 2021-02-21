fb70ea20ac325a58c32e04151e313d04ed4fbb02
Lifoz <lizgfn.@outlook.com>
2021-02-21 18:51:10 +0800

    完成 withCheckComment，尚未测试

--- src/JSONDat.hs
+++ src/JSONDat.hs
@@ -1,12 +1,37 @@
-module JSONDat (JDat(..)) where
+module JSONDat where
 
-data JDat = JNothing                   -- ^ 空值
-          | JNumber Float              -- ^ 数字
-          | JString { jstr :: String } -- ^ 字符串
-          | JComment String            -- ^ 注释
-          | JBool Bool                 -- ^ 布尔值，即 true | false
-          | JNull                      -- ^ 字面量 null
-          | JArray [JDat]              -- ^ 数组，即 [JDat, JDat, ..]
-          | JObject [(String, JDat)]   -- ^ 对象，即 {JString:JDat, JString:JDat, ..}
+data JComment = JInlineComment String
+              | JMultilineComment String
+              | JNoComment
+              deriving (Eq, Ord, Show)
+
+data JAtom a = JAtom
+               { jprec  :: JComment
+               , jval   :: a
+               , jpostc :: JComment}
+               deriving (Eq, Ord, Show)
+
+data JDat = JNothing                         -- ^ 空值
+          | JNumber (JAtom Float)            -- ^ 数字
+          | JString { jstr :: JAtom String } -- ^ 字符串
+          | JBool   (JAtom Bool)             -- ^ 布尔值，即 true | false
+          | JNull   (JAtom ())               -- ^ 字面量 null
+          | JArray  (JAtom [JDat])           -- ^ 数组，即 [JDat, ..]
+          | JObject (JAtom [(String, JDat)]) -- ^ 对象，即 {JString:JDat, ..}
           deriving (Eq, Ord, Show)
+
+instance Functor JAtom where
+  f `fmap` a = a { jval = f $ jval a }
+
+annotate :: JDat     -- ^ 语法结构
+        -> JComment -- ^ 前缀注释
+        -> JComment -- ^ 后缀注释
+        -> JDat     -- ^ 新的语法结构
+annotate (JNumber (JAtom _ x _)) c₁ c₂ = JNumber (JAtom c₁ x c₂)
+annotate (JString (JAtom _ x _)) c₁ c₂ = JString (JAtom c₁ x c₂)
+annotate (JBool   (JAtom _ x _)) c₁ c₂ = JBool   (JAtom c₁ x c₂)
+annotate (JNull   (JAtom _ x _)) c₁ c₂ = JNull   (JAtom c₁ x c₂)
+annotate (JArray  (JAtom _ x _)) c₁ c₂ = JArray  (JAtom c₁ x c₂)
+annotate (JObject (JAtom _ x _)) c₁ c₂ = JObject (JAtom c₁ x c₂)
+annotate x                       _  _  = x
 
--- src/JSONReader.hs
+++ src/JSONReader.hs
@@ -33,27 +33,12 @@ p₁ <|> p₂ = whenOk $ \state₀@(Ok _) ->
        _       -> put state₁ >> return result
 
 constantParser, numberParser, stringParser :: JState
-commentParser                              :: JState
 arrayParser, objectParser                  :: JState
 jValueParser                               :: JState
 
-commentParser = whenOk $ \ (Ok dat) ->
-  case dat of
-    '/':'/':r -> commit $ inlineParser r
-    '/':'*':r -> commit $ multilineParser r
-    _         -> unknown
-  where inlineParser []          = (Ok [], [])
-        inlineParser ('\n':rest) = (Ok rest, [])
-        inlineParser (c:rest)    = (c:) `second` inlineParser rest
-        multilineParser []             = (Fatal "存在未闭合的多行注释", [])
-        multilineParser ('*':'/':rest) = (Ok rest, [])
-        multilineParser (c:rest)       = (c:) `second` multilineParser rest
-        commit :: (ParsingFlag, String) -> JState
-        commit (s@(Ok _), r) = put s >> return (JComment r)
-        commit (s, r)        = put s >> return JNothing
 
-jValueParser = objectParser <|> arrayParser <|>
-             stringParser <|> numberParser <|> constantParser
+jValueParser = objectParser <|> arrayParser  <|>
+               stringParser <|> numberParser <|> constantParser
 
 arrayParser = whenOk $ \ (Ok dat) ->
   case dat of '[':rest -> apaux $ skipSpace rest
@@ -93,7 +78,7 @@ objectParser = whenOk $ \ (Ok dat) ->
 
 
 
-numberParser = whenOk $ \state₀@(Ok dat) ->
+numberParser = withCheckComment $ \state₀@(Ok dat) ->
   case readFloat dat of
     Nothing             -> unknown
     Just (result, rest) -> if not (null rest) && isAlpha (head rest)
@@ -135,7 +120,13 @@ shorten = add . takeWhile (not.isSpace) . take 5
 skipSpace :: String -> String
 skipSpace = dropWhile isSpace
 
-die :: String -> JState
+skipSpacer :: JParsingState ()
+skipSpacer = whenOk $ \ (Ok dat) ->
+  if isSpace (head dat)
+  then put (Ok $ skipSpace dat)
+  else return ()
+
+die :: ErrorMsg -> JState
 die msg = put (Fatal msg) >> return JNothing
 
 unknown :: JState
@@ -147,3 +138,46 @@ whenOk f = get >>= \state₀ ->
     Ok _ -> f state₀
     err  -> put err >> return JNothing
 
+
+-- | 提取注释，不能用于解析器的组合当发现多行注释未闭合时会设置 Fatal
+-- 状态。不会返回 Unknown。不会在内部调用 skipSpace。
+commentParser :: JParsingState JComment
+commentParser = whenOk $ \ (Ok dat) ->
+  commit $ case dat of
+             '/':'/':r -> inlineParser r
+             '/':'*':r -> multilineParser r
+             _         -> (Ok dat, JNoComment)
+  where inlineParser []          = (Ok [], JInlineComment [])
+        inlineParser ('\n':rest) = (Ok rest, JInlineComment [])
+        inlineParser (c:rest)    = consc c `second` inlineParser rest
+        multilineParser [] = (Fatal "存在未闭合的多行注释", JNoComment)
+        multilineParser ('*':'/':rest) = (Ok rest, JMultilineComment [])
+        multilineParser (c:rest)       = consc c `second` multilineParser rest
+        consc c (JInlineComment ic)    = JInlineComment (c:ic)
+        consc c (JMultilineComment mc) = JMultilineComment (c:mc)
+        consc _                        = id
+        commit (s, a) = put s >> return a
+
+-- | 接受一个解析函数，在运行这个函数之前和之后检查表达式周围的注释。
+-- 约定函数总是接受一个 'Ok' 状态，返回一个 'JState'，这个 'JState' 不
+-- 负责解析（周围的）注释，即使解析了也会被覆盖掉。如果注释解析失败
+-- （即 'Fatal'），函数会忽略参数函数的解析结果直接返回错误信息。注释
+-- 的具体内容参见 'JSONDat.JComment'
+--
+-- 这个函数不会调用仅会在注释周围调用 'skipSpace'，不会在调用结束时，
+-- 也就是尾部注释之后调用 'skipSpace'
+withCheckComment :: (ParsingFlag -> JState) -> JState
+-- withCheckComment f = whenOk $ \ state₀@(Ok dat) ->
+--   let (state₁, preCmt) = parseComment dat
+--       (result, state₂) = runState (whenOk f) state₁
+--   in case state₂ of
+--     Unknown   -> unknown
+--     Fatal msg -> die msg
+--     Ok rest -> let (state₃, postCmt) = parseComment rest
+--                in case state₃ of
+--                     Fatal msg' -> die msg'
+--                     Ok _ -> put state₃ >> return (annotate result preCmt postCmt)
+withCheckComment f = commentParser  >>=           \ cmt₁   ->
+  whenOk $ \ _ -> skipSpacer >> get >>= f     >>= \ result ->
+  whenOk $ \ _ -> skipSpacer >> commentParser >>= \ cmt₂   ->
+  whenOk $ \ _ -> return (annotate result cmt₁ cmt₂)
