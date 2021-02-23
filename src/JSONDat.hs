75cf1d414558d41af2faeade7bb42f2d91310c46
Lifoz <lizgfn.@outlook.com>
2021-02-23 17:41:36 +0800

    完成注释重构，取消空注释，改为使用注释列表。尚未测试。

--- src/JSONDat.hs
+++ src/JSONDat.hs
@@ -1,32 +1,56 @@
 module JSONDat where
 
+-- | 代表JSON里的注释，其定义主要是为了区分不同的注释风格。其值分为行
+-- 内注释，多行注释。
 data JComment = JInlineComment String
               | JMultilineComment String
               | JNoComment
               deriving (Eq, Ord, Show)
 
+-- | 象征所有具有实际意义的JSON值。这个数据结构的主要意义是记录注释。
+-- 考虑到一段JSON代码中的任何地方都可以插入注释，并且去掉任何地方的注
+-- 释不会影响这段代码的求值，所以任意一段注释一定在一个JSON数据结构的
+-- 之前或者之后。在此之上，一段注释可以是连续的多个注释，并且注释风格
+-- （行内注释或者多行注释）可能不同，所以每个数据结构由前缀注释、实际
+-- 值、后缀注释组成，其中注释可能为空。另外，这里对注释的判断只是为了
+-- 定位注释而非为了在语法上解析注释，因此一段为下方代码做的注释，可能
+-- 被解析到上方的代码中。
 data JAtom a = JAtom
-               { jprec  :: JComment
-               , jval   :: a
-               , jpostc :: JComment}
-               deriving (Eq, Ord, Show)
+               { jprec  :: [JComment] -- ^ 实际值之前的注释
+               , jval   :: a          -- ^ 实际包含的值
+               , jpostc :: [JComment] -- ^ 实际值之后的注释
+               } deriving (Eq, Ord, Show)
 
-data JDat = JNothing                         -- ^ 空值
-          | JNumber (JAtom Float)            -- ^ 数字
-          | JString { jstr :: JAtom String } -- ^ 字符串
-          | JBool   (JAtom Bool)             -- ^ 布尔值，即 true | false
-          | JNull   (JAtom ())               -- ^ 字面量 null
-          | JArray  (JAtom [JDat])           -- ^ 数组，即 [JDat, ..]
-          | JObject (JAtom [(String, JDat)]) -- ^ 对象，即 {JString:JDat, ..}
+-- | 代表实际的JSON数据。除了特殊值空值以外，所有值都是对'JAtom'值的包
+-- 装。这里定义的JSON值分为：
+--
+--  [@JNumber@] 数字，内部使用一个浮点数表示，还不能分辨不同的数字类型
+--  [@JString@] 字符串，现在仅接受由双引号包裹的字符串
+--  [@JBool@]   布尔值，包含 /true/ 和 /false/ 两个字面量
+--  [@JNull@]   NULL值，即字面量 /null/
+--  [@JArray@]  数组，即由方括号包裹的、逗号分隔的一组JSON值
+--  [@JObject@] 对象，即由方括号包裹的、逗号分隔的、一组序对，每个序对
+--  的左侧为一个字符串，右侧为一个任意JSON值。
+--
+-- 其中数组和对象为复合数据，数据结构中的项都由逗号分隔。约定逗号不出
+-- 现在最后一项之后。
+data JDat = JNothing                               -- ^ 空值
+          | JNumber (JAtom Float)                  -- ^ 数字
+          | JString { jstr :: JAtom String }       -- ^ 字符串
+          | JBool   (JAtom Bool)                   -- ^ 布尔值，即 true | false
+          | JNull   (JAtom ())                     -- ^ 字面量 null
+          | JArray  (JAtom [JDat])                 -- ^ 数组，即 [JDat, ..]
+          | JObject (JAtom [(JAtom String, JDat)]) -- ^ 对象，即 {JString:JDat, ..}
           deriving (Eq, Ord, Show)
 
 instance Functor JAtom where
   f `fmap` a = a { jval = f $ jval a }
 
-annotate :: JDat     -- ^ 语法结构
-        -> JComment -- ^ 前缀注释
-        -> JComment -- ^ 后缀注释
-        -> JDat     -- ^ 新的语法结构
+-- | 为一个JSON数据结构添加注释
+annotate :: JDat      -- ^ 语法结构
+        -> [JComment] -- ^ 前缀注释
+        -> [JComment] -- ^ 后缀注释
+        -> JDat       -- ^ 新的语法结构
 annotate (JNumber (JAtom _ x _)) c₁ c₂ = JNumber (JAtom c₁ x c₂)
 annotate (JString (JAtom _ x _)) c₁ c₂ = JString (JAtom c₁ x c₂)
 annotate (JBool   (JAtom _ x _)) c₁ c₂ = JBool   (JAtom c₁ x c₂)
@@ -35,3 +59,12 @@ annotate (JArray  (JAtom _ x _)) c₁ c₂ = JArray  (JAtom c₁ x c₂)
 annotate (JObject (JAtom _ x _)) c₁ c₂ = JObject (JAtom c₁ x c₂)
 annotate x                       _  _  = x
 
+plainAtom n = JAtom { jprec  = []
+                    , jval   = n
+                    , jpostc = [] }
+plainJNumber = JNumber . plainAtom
+plainJString = JString . plainAtom
+plainJBool   = JBool   . plainAtom
+plainJArray  = JArray  . plainAtom
+plainJObject = JObject . plainAtom
+plainJNull   = JNull (plainAtom ())
--- src/JSONReader.hs
+++ src/JSONReader.hs
@@ -40,71 +40,95 @@ jValueParser                               :: JState
 jValueParser = objectParser <|> arrayParser  <|>
                stringParser <|> numberParser <|> constantParser
 
-arrayParser = whenOk $ \ (Ok dat) ->
-  case dat of '[':rest -> apaux $ skipSpace rest
+-- | 提取注释，不能用于解析器的组合当发现多行注释未闭合时会设置 Fatal
+-- 状态。不会返回 Unknown。不会在内部调用 skipSpace。
+commentParser :: JParsingState [JComment]
+commentParser = maybeOk [] $ \ state₀@(Ok dat) ->
+  case dat of
+    '/':'/':r -> makComment (inlineParser r) >>= \ cmt ->
+      first (JInlineComment cmt:) `mapState` commentParser
+    '/':'*':r -> makComment (multilineParser r) >>= \ cmt ->
+      first (JMultilineComment cmt:) `mapState` commentParser
+    _         -> return []
+  where inlineParser, multilineParser :: String -> (String, ParsingFlag)
+        makComment :: (String, ParsingFlag) -> JParsingState String
+        inlineParser []          = ([], Ok [])
+        inlineParser ('\n':rest) = ([], Ok rest)
+        inlineParser (c:rest)    = (c:) `first` inlineParser rest
+        multilineParser []             = ([], Fatal "存在未闭合的多行注释")
+        multilineParser ('*':'/':rest) = ([], Ok rest)
+        multilineParser (c:rest)       = (c:) `first` multilineParser rest
+        makComment (c, Ok s) = put (Ok $ skipSpace s) >> return c
+        makComment (_, err)  = put err >> return []
+
+-- | 接受一个解析函数，在运行这个函数之前和之后检查表达式周围的注释。
+-- 约定函数总是接受一个 'Ok' 状态，返回一个 'JState'，这个 'JState' 不
+-- 负责解析（周围的）注释，即使解析了也会被覆盖掉。如果注释解析失败
+-- （即 'Fatal'），函数会忽略参数函数的解析结果直接返回错误信息。注释
+-- 的具体内容参见 'JSONDat.JComment'
+--
+-- 这个函数不会调用仅会在注释周围调用 'skipSpace'，不会在调用结束时，
+-- 也就是尾部注释之后调用 'skipSpace'
+withCheckComment :: (ParsingFlag -> JState) -> JState
+withCheckComment f = commentParser  >>=           \ cmt₁   ->
+  whenOk $ \ _ -> skipSpacer >> get >>= f     >>= \ result ->
+  whenOk $ \ _ -> skipSpacer >> commentParser >>= \ cmt₂   ->
+  whenOk $ \ _ -> return (annotate result cmt₁ cmt₂)
+
+
+arrayParser = withCheckComment $ \ (Ok dat) ->
+  case dat of '[':rest -> put (Ok rest) >> (mapState $ first plainJArray) apaux
               _        -> unknown
-  where apaux :: String -> JState
-        apaux [] = die "存在未闭合的数组"
-        apaux (']':r) = put (Ok r) >> return (JArray [])
-        apaux s₀      = put (Ok s₀) >> jValueParser >>= \ result ->
-          whenOk $ \ (Ok dat) ->
-                     case skipSpace dat of
-                       s₁@(c:cs) -> let rest = if c == ',' then cs else s₁
-                                    in apaux (skipSpace rest) >>= cons result
-                       r -> apaux (skipSpace r)
-        cons result (JArray jar) = return (JArray $ result:jar)
-        cons _       JNothing    = return  JNothing
+  where apaux    :: JParsingState [JDat]
+        extractE :: ParsingFlag -> JParsingState [JDat]
+        apaux = maybeOk [] extractE
+        extractE (Ok [])         = put (Fatal "存在未闭合的数组") >> return []
+        extractE (Ok (']':rest)) = put (Ok rest)                  >> return []
+        extractE _  = jValueParser >>= \ elt ->
+          ignoreComma ']' >> apaux >>= \ rest ->
+          return $ elt:rest
 
-objectParser = whenOk $ \ (Ok dat) ->
-  case dat of '{':rest -> opaux $ skipSpace rest
+objectParser = withCheckComment $ \ (Ok dat) ->
+  case dat of '{':rest -> put (Ok rest) >> (mapState $ first plainJObject) opaux
               _        -> unknown
-  where opaux [] = die "存在未闭合的对象"
-        opaux ('}':r) = put (Ok r) >> return (JObject [])
-        opaux s₀      = put (Ok s₀) >> stringParser >>= \ keyr ->
-          nextCharShould (==':')    >> jValueParser   >>= \ valr ->
-          whenOk $ \ (Ok dat) ->
-                     case skipSpace dat of
-                       s₁@(c:cs) | c == ',' ->
-                                   opaux (skipSpace cs) >>= cons (jstr keyr, valr)
-                       s₁ -> nextCharShould (=='}') >>=
-                         const (whenOk . const . return $ JObject [(jstr keyr, valr)])
-        cons kv (JObject r) = return (JObject $ kv:r)
-        cons _  JNothing    = return JNothing
-        nextCharShould :: (Char -> Bool) -> JState
-        nextCharShould pred = whenOk $ \ (Ok dat) ->
-          case skipSpace dat of
-            c:cs | pred c -> put (Ok $ skipSpace cs) >> return JNothing
-            _             -> die ("存在语法错误 " ++ shorten dat)
-
-
+  where extractO :: ParsingFlag -> JParsingState [(JAtom String, JDat)]
+        opaux    :: JParsingState [(JAtom String, JDat)]
+        opaux = maybeOk [] extractO
+        extractO (Ok [])        = put (Fatal "存在未闭合的对象") >> return []
+        extractO (Ok ('}':r))   = put (Ok r)                     >> return []
+        extractO _              =   stringParser >>= \ keyr  ->
+          nextCharShould (==':') >> jValueParser >>= \ valr  ->
+          ignoreComma        '}' >> opaux        >>= \ restr ->
+          return $ (jstr keyr, valr):restr
 
 numberParser = withCheckComment $ \state₀@(Ok dat) ->
   case readFloat dat of
-    Nothing             -> unknown
-    Just (result, rest) -> if not (null rest) && isAlpha (head rest)
-                           then die $ "数字后有非法字母 " ++ shorten rest
-                           else put (Ok rest) >> return (JNumber result)
+    Nothing                         -> unknown
+    Just (_, rest@(c:_)) | isAlpha c -> die $ "数字后有非法字母 " ++ shorten rest
+    Just (result, rest)             -> put (Ok rest) >> return (plainJNumber result)
   where readFloat :: String -> Maybe (Float, String)
         readFloat s₀ = case reads s₀ :: [(Float, String)] of
                          []  -> Nothing
                          r:_ -> Just r
 
-stringParser = whenOk $ \state₀@(Ok dat) ->
+stringParser = withCheckComment $ \state₀@(Ok dat) ->
   case dat of '\"':rest -> spaux rest
               _         -> unknown
   where spaux :: String -> JState
         spaux s = maybe (die "存在未闭合的字符串") str2jstate (extraString s)
         str2jstate :: (String, String) -> JState
-        str2jstate (str, rest) = put (Ok rest) >> return (JString str)
+        str2jstate (str, rest) = put (Ok rest) >> return (plainJString str)
 
-constantParser = whenOk $ \state₀@(Ok dat) ->
+constantParser = withCheckComment $ \state₀@(Ok dat) ->
   case find (\ (k, _) -> k `isPrefixOf` dat) alist of
     Nothing     -> unknown
     Just (k, v) -> case drop (length k) dat of
-                     c:cs | isAlpha c || c == '_' ->
+                     c:cs | isAlphaNum c || c == '_' ->
                             die $ "存在未知的字面量 " ++ shorten dat
                      dat' -> put (Ok dat') >> return v
-  where alist = [ ("true", JBool True), ("false", JBool False), ("null", JNull)]
+  where alist = [ ("true", plainJBool True)
+                , ("false", plainJBool False)
+                , ("null", plainJNull)]
 
 extraString :: String -> Maybe (String, String)
 extraString s₀ = case break (\c -> c == '"' || c == '\\') s₀ of
@@ -121,10 +145,7 @@ skipSpace :: String -> String
 skipSpace = dropWhile isSpace
 
 skipSpacer :: JParsingState ()
-skipSpacer = whenOk $ \ (Ok dat) ->
-  if isSpace (head dat)
-  then put (Ok $ skipSpace dat)
-  else return ()
+skipSpacer = maybeOk () $ \ (Ok s) -> put . Ok . skipSpace $ s
 
 die :: ErrorMsg -> JState
 die msg = put (Fatal msg) >> return JNothing
@@ -132,52 +153,26 @@ die msg = put (Fatal msg) >> return JNothing
 unknown :: JState
 unknown = put Unknown >> return JNothing
 
-whenOk :: (ParsingFlag -> JState) -> JState
-whenOk f = get >>= \state₀ ->
+maybeOk :: a -> (ParsingFlag -> JParsingState a) -> JParsingState a
+maybeOk d f = get >>= \state₀ ->
   case state₀ of
     Ok _ -> f state₀
-    err  -> put err >> return JNothing
+    err  -> put err >> return d
+
+whenOk :: (ParsingFlag -> JState) -> JState
+whenOk = maybeOk JNothing
 
 
--- | 提取注释，不能用于解析器的组合当发现多行注释未闭合时会设置 Fatal
--- 状态。不会返回 Unknown。不会在内部调用 skipSpace。
-commentParser :: JParsingState JComment
-commentParser = whenOk $ \ (Ok dat) ->
-  commit $ case dat of
-             '/':'/':r -> inlineParser r
-             '/':'*':r -> multilineParser r
-             _         -> (Ok dat, JNoComment)
-  where inlineParser []          = (Ok [], JInlineComment [])
-        inlineParser ('\n':rest) = (Ok rest, JInlineComment [])
-        inlineParser (c:rest)    = consc c `second` inlineParser rest
-        multilineParser [] = (Fatal "存在未闭合的多行注释", JNoComment)
-        multilineParser ('*':'/':rest) = (Ok rest, JMultilineComment [])
-        multilineParser (c:rest)       = consc c `second` multilineParser rest
-        consc c (JInlineComment ic)    = JInlineComment (c:ic)
-        consc c (JMultilineComment mc) = JMultilineComment (c:mc)
-        consc _                        = id
-        commit (s, a) = put s >> return a
-
--- | 接受一个解析函数，在运行这个函数之前和之后检查表达式周围的注释。
--- 约定函数总是接受一个 'Ok' 状态，返回一个 'JState'，这个 'JState' 不
--- 负责解析（周围的）注释，即使解析了也会被覆盖掉。如果注释解析失败
--- （即 'Fatal'），函数会忽略参数函数的解析结果直接返回错误信息。注释
--- 的具体内容参见 'JSONDat.JComment'
---
--- 这个函数不会调用仅会在注释周围调用 'skipSpace'，不会在调用结束时，
--- 也就是尾部注释之后调用 'skipSpace'
-withCheckComment :: (ParsingFlag -> JState) -> JState
--- withCheckComment f = whenOk $ \ state₀@(Ok dat) ->
---   let (state₁, preCmt) = parseComment dat
---       (result, state₂) = runState (whenOk f) state₁
---   in case state₂ of
---     Unknown   -> unknown
---     Fatal msg -> die msg
---     Ok rest -> let (state₃, postCmt) = parseComment rest
---                in case state₃ of
---                     Fatal msg' -> die msg'
---                     Ok _ -> put state₃ >> return (annotate result preCmt postCmt)
-withCheckComment f = commentParser  >>=           \ cmt₁   ->
-  whenOk $ \ _ -> skipSpacer >> get >>= f     >>= \ result ->
-  whenOk $ \ _ -> skipSpacer >> commentParser >>= \ cmt₂   ->
-  whenOk $ \ _ -> return (annotate result cmt₁ cmt₂)
+nextCharShould :: (Char -> Bool) -> JState
+nextCharShould pred = whenOk $ \ (Ok dat) ->
+  case skipSpace dat of
+    c:cs | pred c -> put (Ok $ skipSpace cs) >> return JNothing
+    cs            -> die ("存在语法错误 " ++ shorten cs)
+ignoreComma :: Char -> JState
+ignoreComma closeSign =
+  skipSpacer >> whenOk checkComma
+  where checkComma (Ok (',':rest)) =
+          case skipSpace rest of
+            c:rest' | c == closeSign -> die $ "存在非法的逗号：, " ++ [c]
+            rest'                    -> put (Ok rest') >> return JNothing
+        checkComma _             = return JNothing
