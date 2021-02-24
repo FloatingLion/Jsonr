3ab87899c0d0f0c61d6170ff370c9e4ef5b39c9d
Lifoz <lizgfn.@outlook.com>
2021-02-24 21:10:26 +0800

    添加 README 文档。

--- src/JSONPrinter/Compress.hs
+++ src/JSONPrinter/Compress.hs
@@ -5,6 +5,7 @@ import           JSONDat    (JAtom (..), JDat (..))
 import           JSONReader (jread)
 
 jprint :: JDat -> String
+-- ^ 压缩格式输出，所有 'JDat' 将以最紧凑的格式转化为字符串。
 jprint (JNull _) = "null"
 jprint (JBool a) = if jval a then "true" else "false"
 jprint (JNumber a) = show $ jval a
